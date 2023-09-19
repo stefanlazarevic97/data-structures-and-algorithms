1c5cc4e91c70c34f2fb8fcbcf3e63736b21c5831
stefanlazarevic <stefanlaza97@gmail.com>
2023-09-19 16:59:07 -0700

    add lucasNumber

--- recursion-project/lib/problems.js
+++ recursion-project/lib/problems.js
@@ -16,8 +16,12 @@
 // lucasNumber(3)   // => 4
 // lucasNumber(5)   // => 11
 // lucasNumber(9)   // => 76
-function lucasNumber(n) {
 
+function lucasNumber(n) {
+    if (n === 0) return 2;
+    if (n === 1) return 1;
+
+    return lucasNumber(n - 1) + lucasNumber(n - 2);
 }
 
 
