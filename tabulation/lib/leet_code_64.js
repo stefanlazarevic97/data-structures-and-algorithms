6956f438b9c7b521360fae195e1f9e062ded1a06
stefanlazarevic <stefanlaza97@gmail.com>
2023-09-21 15:18:59 -0700

    add minPathSum

--- tabulation/lib/leet_code_64.js
+++ tabulation/lib/leet_code_64.js
@@ -1,6 +1,24 @@
 // Work through this problem on https://leetcode.com/problems/minimum-path-sum/ and use the specs given there.
 // Feel free to use this file for scratch work.
 
-function minPathSum(grid) {
+var minPathSum = function (grid) {
+    let rows = grid.length;
+    let cols = grid[0].length;
+    let table = new Array(rows).fill().map(() => new Array(cols).fill(Infinity));
 
-}
+    table[0][0] = grid[0][0];
+
+    for (let i = 0; i < rows; i++) {
+        for (let j = 0; j < cols; j++) {
+            if (j < cols - 1) {
+                table[i][j + 1] = Math.min(table[i][j] + grid[i][j + 1], table[i][j + 1])
+            }
+            
+            if (i < rows - 1) {
+                table[i + 1][j] = Math.min(table[i][j] + grid[i + 1][j], table[i + 1][j])
+            }
+        }
+    }
+
+    return table[rows - 1][cols - 1];
+};
